5b8e09a663d3b8da123c2eadb1685259fc23d8f0
asrith-raju <asrithraju143@gmail.com>
2025-12-20 19:06:13 +0530

    client(feat): create callToAction content

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const CallToAction = () => {
   return (
     <div>
-      <h1>Call to Action</h1>
-    </div>
+      <h1>Learn anything,anytime,anywhere</h1>
+      <p>Incididunt sint fugiat pariatur cupidatat consectetur sit cillium anim id venim aliqua prodient execpteur commodo doea.</p>
+      <div>
+        <button>Get Started</button>
+        <button>Learn more <img src={assets.arrow_icon} alt="arrow_icon" /></button>
+      </div>
+    </div> 
   )
 }
 
