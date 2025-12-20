56f67d319a467a310d5a3a5dc7ad8e7a7a4041d6
asrith-raju <asrithraju143@gmail.com>
2025-12-20 20:15:59 +0530

    client(feat): add tailwind classes to footer

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -3,16 +3,27 @@ import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-          <div>
+    <footer className='bg-gray-900 md:px-36 text-left w-full mt-10'>
+      <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border border-white/30'>
+          <div className='flex flex-col md:items-center md:text-left text-sm text-white/80'>
             <img src={assets.logo_dark} alt="logo" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consequatur tempore at distinctio soluta delectus quae facilis illum, ab obcaecati.</p>
+            <p className='mt-6 text-center md:text-left text-sm text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consequatur tempore at distinctio soluta delectus quae facilis illum, ab obcaecati.</p>
           </div>
-          <div></div>
-          <div></div>
+          <div className='flex flex-col md:items-start items-center w-full'>
+            <h2 className='font-semibold text-white mb-5'>Company</h2>
+            <ul className='flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+          <div className='hidden md:flex flex-col items-start w-full'>
+            <h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
+            <p className='text-sm text-white/80'>The latest news, articles, and resources, sent to your inbox weekly.</p>
+          </div>
       </div>
-      <p>Copyright 2025 @ GreatStack.All Right Reserved.</p>
+      <p className='py-4 text-center text-xs md:text-sm text-white/80'>Copyright 2025 @ GreatStack.All Right Reserved.</p>
     </footer>
   )
 }
