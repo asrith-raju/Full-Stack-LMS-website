bad636674c7ea2415e268969f350aff37911d7fe
asrith-raju <asrithraju143@gmail.com>
2025-12-21 17:51:02 +0530

    client(feat): add filtered course functionality in CourseList page

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../../context/AppContext'
 import SearchBar from '../../components/student/SearchBar'
 import { data, useParams } from 'react-router-dom'
@@ -6,8 +6,28 @@ import CourseCard from '../../components/student/CourseCard'
 
 const CoursesList = () => {
   const { navigate,allCourses} = useContext(AppContext)
-
+  
   const {input}= useParams()
+
+  const [filteresCourse,setfilteredCourse] = useState([])
+
+  useEffect(() => {
+    
+   if(allCourses && allCourses.length >0){
+    const tempCourses = allCourses.slice()
+    input ? 
+        setfilteredCourse(
+          tempCourses.filter(
+            item=>item.courseTitle.toLowerCase().includes(input.toLowerCase())
+          )
+        )
+    : setfilteredCourse(tempCourses)
+   }
+
+  }, [allCourses,input])
+  
+
+
   return (
     <>
       <div className='relative md:px-36 px-8 pt-20 text-left'>
@@ -21,7 +41,7 @@ const CoursesList = () => {
         </div>
 
         <div className='grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
-           {allCourses.map((course,index)=><CourseCard key={index} course={course}/>)}
+           {filteresCourse.map((course,index)=><CourseCard key={index} course={course}/>)}
         </div>
       </div>
     </>
