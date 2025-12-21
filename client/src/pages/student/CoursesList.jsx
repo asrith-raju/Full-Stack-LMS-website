49e5d3c571339dc4a4556ff3a511f8073157929f
asrith-raju <asrithraju143@gmail.com>
2025-12-21 17:27:29 +0530

    client(feat): add CourseList content

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
+import SearchBar from '../../components/student/SearchBar'
+import { data, useParams } from 'react-router-dom'
 
 const CoursesList = () => {
+  const { navigate } = useContext(AppContext)
+
+  const {input}= useParams()
   return (
-    <div>
-      <h1>Courses List Page</h1>
-    </div>
+    <>
+      <div>
+        <div>
+          <div>
+            <h1>Course List</h1>
+            <p className='text-gray-500'>
+              <span className='text-blue-600 cursor-pointer' onClick={() => navigate('/')}>Home</span> / <span>Course List</span></p>
+          </div>
+        <SearchBar data={data}/>
+        </div>
+      </div>
+    </>
   )
 }
 
