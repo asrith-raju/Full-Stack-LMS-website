6fc198c5ff158d37ca5832e6a6bdac4e08037a46
asrith-raju <asrithraju143@gmail.com>
2025-12-22 17:13:50 +0530

    client(feat): add fetchCourseData function in CourseDetails page

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useState,useEffect } from 'react'
+import {useParams} from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 
 const CourseDetails = () => {
+
+  const {id} = useParams()
+
+  const [courseData,setCourseData] = useState(null)
+
+  const {allCourses} = useContext(AppContext)
+
+
+  const fetchCourseData = ()=>{
+   const findCourse = allCourses.find(course => course._id ===id)
+   setCourseData(findCourse)
+  }
+
+  useEffect(() => {
+   fetchCourseData( )
+  }, [])
+  
   return (
     <div>
       <h1>course details  page</h1>
