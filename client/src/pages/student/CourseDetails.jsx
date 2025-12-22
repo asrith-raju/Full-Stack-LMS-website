a8c7c67fec5fecb9369e96c9e7e88847d4261cad
asrith-raju <asrithraju143@gmail.com>
2025-12-22 21:56:49 +0530

    client(feat): add headings and ratings section in CourseDetails page

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -3,6 +3,7 @@ import { useState,useEffect } from 'react'
 import {useParams} from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/student/Loading'
+import { assets } from '../../assets/assets'
 
 const CourseDetails = () => {
 
@@ -10,7 +11,7 @@ const CourseDetails = () => {
 
   const [courseData,setCourseData] = useState(null)
 
-  const {allCourses} = useContext(AppContext)
+  const {allCourses,calculateRating} = useContext(AppContext)
 
 
   const fetchCourseData = ()=>{
@@ -29,9 +30,25 @@ const CourseDetails = () => {
 
       </div>
     {/* left column */}
-    <div>
-        <h1>{courseData.courseTitle}</h1>
-        <p>{courseData.courseDescription}</p>
+    <div className='max-w-xl z-10 text-gray-500'>
+        <h1 className='md:text-course-details-heading-large text-course-details-heading-small
+        font-semibold text-gray-800'>{courseData.courseTitle}</h1>
+        <p  className='pt-4 md:text-base text-sm'
+        dangerouslySetInnerHTML={{__html:courseData.courseDescription.slice(0,200)}}></p>
+
+        {/* review and rating */}
+          <div className='flex items-center space-x-2 pt-3 pb-1 text-sm'>
+                   <p>{calculateRating(courseData)}</p>
+                   <div className='flex'>
+                     {[...Array(5)].map((_,i)=>(
+                       <img className='w-3.5 h-3.5' src={i<Math.floor(calculateRating(courseData))?assets.star : assets.star_blank} key={i} alt=''/>
+                     ))}
+                   </div>
+                   <p className='text-blue-600'>({courseData.courseRatings.length}  {courseData.courseRatings.length > 1 ?  'ratings':'rating'})</p> 
+                   <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length > 1 ? 'students' :'student'}</p>
+                 </div>
+                <p className='text-sm'>Course by <span className='text-blue-600 underline'>Asrith Raju</span></p> 
+          
     </div>
     {/* Right column */}
     <div>
