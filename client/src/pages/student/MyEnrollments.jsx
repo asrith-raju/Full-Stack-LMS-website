541c7ea3f70cac63247bb8afbc73bfc599055a22
asrith-raju <asrithraju143@gmail.com>
2025-12-25 20:22:57 +0530

    client(feat): add table headings in MyEnrollments page

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const MyEnrollments = () => {
   return (
+    <>
     <div>
-      <h1>My enrollments page</h1>
+      <h1>My Enrollments</h1>
+      <table>
+        <thead>
+          <tr>
+            <th className='px-4 py-3 font-semibold truncate'>Course</th>
+            <th className='px-4 py-3 font-semibold truncate'>Duration</th>
+            <th className='px-4 py-3 font-semibold truncate'>Completed</th>
+            <th className='px-4 py-3 font-semibold truncate'>Status</th>
+          </tr>
+        </thead>
+      </table>
     </div>
+    </>
   )
 }
 
