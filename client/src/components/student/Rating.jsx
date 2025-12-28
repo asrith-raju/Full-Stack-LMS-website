3a826e1757db463cb0790a4f15b530e6e477f6b6
asrith-raju <asrithraju143@gmail.com>
2025-12-28 22:04:12 +0530

    client(feat): link handleRating function to starvalue

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -17,7 +17,8 @@ useEffect(()=>{
      {Array.from({length:5},(_,index)=>{
        const starValue = index+1
        return (
-        <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500':'text-gray-400'}`}> 
+        <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500':'text-gray-400'}`}
+        onClick={()=>handleRating(starValue)}> 
           &#9733;
         </span>
        )
