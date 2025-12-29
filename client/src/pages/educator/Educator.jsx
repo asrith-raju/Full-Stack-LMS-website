732551b33a7700b830342a993418da71d79d94f2
asrith-raju <asrithraju143@gmail.com>
2025-12-29 17:53:03 +0530

    client(feat): Mount SideBar in the Educator page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
+import SideBar from '../../components/educator/SideBar'
 
 const Educator = () => {
   return (
     <div className='text-default min-h-screen bg-white'>
       <Navbar/>
-      <div> 
+      <div className='flex'> 
+        <SideBar/>
+        <div className='flex-1'>
         {<Outlet/>} 
+        </div>
         </div>
     </div>
   )
