9abeb411cc4aaee410c2f17ac2b97dc700e13771
asrith-raju <asrithraju143@gmail.com>
2025-12-29 17:17:16 +0530

    client(feat): add Profile image and UserButton in Navbar

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -7,11 +7,14 @@ const Navbar = () => {
   const educatorData = dummyEducatorData
   const {user} = useUser() 
   return (
-    <div>
+    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3'>
       <Link to='/'>
      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32' />
      </Link>
-     
+     <div className='flex items-center gap-5 text-gray-500 relative'>
+      <p>Hi! {user ? user.fullName : 'Developers'}</p>
+      {user ? <UserButton/>: <img src={assets.profile_img} alt="" className='max-w-8'/> }
+     </div>
     </div>
   )
 }
