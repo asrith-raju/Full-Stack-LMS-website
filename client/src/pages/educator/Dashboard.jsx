d74821892ea94818dfd8f296e3f25acef3788633
asrith-raju <asrithraju143@gmail.com>
2025-12-30 18:04:00 +0530

    client(feat): add LatestEnrollments div

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState,useEffect } from 'react'
 import { AppContext } from '../../context/AppContext'
 import { assets, dummyDashboardData } from '../../assets/assets'
 import Loading from '../../components/student/Loading'
@@ -17,16 +17,17 @@ const Dashboard = () => {
   }, [])
   
   return dashboardData ? (
-    <div className='min-h-screen flex flex-col items-center justify-between gap-8 md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+    <div className='min-h-screen flex flex-col items-start justify-between gap-8 md:p-8 md:pb-0 p-4 pt-8 pb-0'>
       <div className='space-y-5'>
           <div className='flex flex-wrap gap-5 items-center'>
                <div className='flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md'>
                 <img src={assets.patients_icon} alt="patients_icon" />
                 <div>
                   <p className='text-2xl font-medium text-gray-600'> {dashboardData.enrolledStudentsData.length}</p>
-                  <p className='text-base text-gray-500'>Total Enrollments</p>
+                  <p className='text-base text-gray-500'>Total Enrolments</p>
                 </div>
                </div>
+
                <div className='flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md'>
                 <img src={assets.appointments_icon} alt="patients_icon" />
                 <div>
@@ -34,6 +35,7 @@ const Dashboard = () => {
                   <p className='text-base text-gray-500'>Total Courses</p>
                 </div>
                </div>
+
                <div className='flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md'>
                 <img src={assets.earning_icon} alt="patients_icon" />
                 <div>
@@ -41,6 +43,12 @@ const Dashboard = () => {
                   <p className='text-base text-gray-500'>Total Earnings</p>
                 </div>
                </div>
+          </div>
+          <div>
+            <h2 className='pb-4 text-lg font-medium'>Latest Enrollments</h2>
+            <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
+                  
+            </div>
           </div>
       </div>
     </div>
