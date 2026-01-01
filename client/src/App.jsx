805472b1b890fb568c9ae921163b68807ea989c3
asrith-raju <asrithraju143@gmail.com>
2026-01-01 16:54:28 +0530

    client(feat): Create input element for CourseTitle in AddCourse page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import MyCourses from './pages/educator/MyCourses'
 import Dashboard from './pages/educator/Dashboard'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
+import "quill/dist/quill.snow.css";
 
 
 
--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,6 +1,7 @@
-import React, { useRef,useState } from 'react'
+import React, { useRef, useState,useEffect} from 'react'
 import uniqid from 'uniqid'
 import Quill from 'quill'
+import { assets } from '../../assets/assets'
 
 const AddCourse = () => {
   const quillRef = useRef(null)
@@ -16,24 +17,35 @@ const AddCourse = () => {
 
   const [lectureDetails, setLectureDetails] = useState(
     {
-      lectureTitle:'',
-      lectureDuration:'',
-      lectureUrl:'',
-      isPreviewFree:false, 
+      lectureTitle: '',
+      lectureDuration: '',
+      lectureUrl: '',
+      isPreviewFree: false,
     }
   )
 
- useEffect(() => {
-   if(!quillRef.current && editorRef.current){
-    quillRef.current = new Quill(editorRef.current,{
-      theme:'snow',
-    }); 
-   }
- }, [])
- 
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: 'snow',
+      });
+    }
+  }, [])
+
   return (
-    <div>
-      
+    <div className='h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+      <form action="" className='flex flex-col gap-4 max-w-md w-full text-gray-500'>
+        <div className='flex flex-col gap-1'>
+          <p>Course Title</p>
+          <input onChange={e => setCourseTitle(e.target.value)} value={courseTitle} type="text" placeholder='Type here' className='outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500' required />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p>Course Description</p>
+          <div ref={editorRef}></div>
+        </div>
+ 
+    
+      </form>
     </div>
   )
 }
