d9ac8e8c64ba43a15a6ffa4dc8119280489968d6
asrith-raju <asrithraju143@gmail.com>
2026-01-05 11:35:04 +0530

    server(feat): add addUserRating controller function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -122,3 +122,33 @@ export const purchaseCourse = async(req,res)=>{
         res.json({success:false,message:error.message})
     }
  }
+
+ //Add User Rating to Course
+    export const addUserRating = async(req,res)=>{
+        const userId = req.auth().userId
+        const {courseId,rating} = req.body
+        if(!courseId || !userId || !rating || rating<1 || rating>5){
+            return res.json({success:false,message:'Invalid Details'})
+        }
+        try {
+            const course = await Course.findById(courseId)
+            if(!course){
+                return res.json({success:false,message:'Course Not Found'})
+            }
+            const user = await User.findById(userId)
+            if(!user || !user.enrolledCourses.includes(courseId)){
+            return res.json({success:false,message:'User has Not Purchased this Course'})
+            }
+
+            const existingRatingIndex = course.courseRatings.findIndex(r=>r.userId === userId)
+            if(existingRatingIndex>-1){
+                course.courseRatings[existingRatingIndex].rating = rating
+            }else{
+                course.courseRatings.push({userId,rating})
+            }
+            await course.save()
+            res.json({success:true,message:'Rating Added Successfully'})
+        } catch (error) {
+            res.json({success:false,message:error.message})
+        }
+    }
